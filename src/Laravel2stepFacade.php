f5268fa24cb71d5fd57d04aa6038a1b31b237910
Jeremy <jeremykenedy@gmail.com>
2020-05-23 15:45:07 -0700

    psr4

--- src/Laravel2stepFacade.php
+++ src/Laravel2stepFacade.php
@@ -7,7 +7,9 @@ use Illuminate\Support\Facades\Facade;
 class Laravel2stepFacade extends Facade
 {
     /**
-     * {@inheritdoc}
+     * Gets the facade accessor.
+     *
+     * @return string The facade accessor.
      */
     protected static function getFacadeAccessor()
     {
